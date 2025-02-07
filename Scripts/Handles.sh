b3bd68827941f7a708d7b764f9fd8662e4f4af7a
ayu-z <yuzuihong@live.com>
2025-02-07 23:42:54 +0800

    me

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -33,13 +33,13 @@ if [ -d *"luci-theme-argon"* ]; then
 fi
 
 #移除Shadowsocks组件
-PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
+PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall2/Makefile")
 if [ -f "$PW_FILE" ]; then
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
 	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
 
-	cd $PKG_PATH && echo "passwall has been fixed!"
+	cd $PKG_PATH && echo "passwall2 has been fixed!"
 fi
 
 SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
